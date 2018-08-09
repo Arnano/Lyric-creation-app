75f23a1062775b1bc5791c7bdf423f16685c964e
Arnaud bourdillon <arnaud.bourdillon@yahoo.fr>
2018-08-09 20:30:14 +0100

    Adds thumbs up

--- client/components/LyricList/index.js
+++ client/components/LyricList/index.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
+  onLike(id) {
+    console.log(id);
+  }
+
   getLyricList() {
     return (
       this.props.lyrics.map(({ id, content }) => {
         return (
-          <li key={id} className='collection-item'> { content } </li>
+          <li
+            key={id}
+            className='collection-item'>
+              { content }
+              <i className='material-icons' onClick={() => this.onLike(id)}>
+                thumb_up
+              </i>
+          </li>
         )}
       )
     );
